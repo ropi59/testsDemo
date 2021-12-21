39cdb59afc8582ac41b0daf97a78485f2737c818
Olivier <mrseed@hotmail.com>
2021-12-21 09:30:48 +0100

    First commit

---
+++ src/main/java/fr/m2i/testsDemo/TestsDemoApplication.java
@@ -0,0 +1,13 @@
+package fr.m2i.testsDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/m2i/testsDemo/TestsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.m2i.testsDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
